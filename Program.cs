8ecc834db0fab03ae7b7dfd3ed3ef839c89f7cc5
Nicolas Bach <nicolas.bach@epitech.eu>
2025-07-15 15:02:37 +0200

    refactor: changing test file (Program.cs)
    Testing the ParkCar and CarLeaves methods

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@ class Program
 {
     static void Main()
     {
-        Parking testParking = new Parking(25);
+        Parking testParking = new Parking(26);
 
         Console.WriteLine("Total slots is:" + Convert.ToString(testParking.totalSlot));
+        testParking.DisplayParking();
+        for (int i = 0; i < 10; i++)
+            testParking.ParkCar();
+        testParking.DisplayParking();
+        testParking.CarLeaves();
         testParking.DisplayParking();
     }
 }

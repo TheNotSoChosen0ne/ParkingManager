3de86d68018b56691b1df6110a6facb62515de2a
Nicolas Bach <nicolas.bach@epitech.eu>
2025-07-15 15:11:53 +0200

    feat: adding Level class
    The purpose of the level class is to contain the slots
    and make the Parking class a HUB of Level instances.
    
    The level class contains the following:
    	- id: an ID (set to 0 by default)
    	- level: the level indicator
    	- totalSlot: the number of slots
    	- availableSlot: the number of available slots

---
+++ level.cs
@@ -0,0 +1,16 @@
+
+class Levels
+{
+    private int _id;
+    public int level { get; private set; }
+    public int totalSlot { get; private set; }
+    public int availableSlot { get; private set; }
+
+    public Levels(int level, int nbrSlot = 100)
+    {
+        this._id = 0;
+        this.level = level;
+        this.totalSlot = nbrSlot;
+        this.availableSlot = nbrSlot;
+    }
+}
